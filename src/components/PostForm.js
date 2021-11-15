f64f13634a908735c57266bf4091d4cc6f2877e2
Edwin <edwin8564@gmail.com>
2021-11-15 20:00:27 +0100

    Allow authenticated user to create posts

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -1,21 +1,52 @@
-import { Navigate, useLocation } from "react-router"
+import { useLocation, useNavigate } from "react-router"
+import { useEffect } from "react";
 
 const PostForm = () => {
     const location = useLocation();
+    let navigate = useNavigate();
     let token;
     if (location.state === null) {
-        <Navigate to="/" />
+        navigate("/");
     } else {
         token = location.state.currentToken;
     }
+
+    useEffect(() => {
+        const form = document.getElementsByTagName("form")[0];
+        form.addEventListener("submit", (e) => {
+          e.preventDefault();
+          let data = {};
+          data.title = document.getElementById("title").value
+          data.text = document.getElementById("text").value
+          data.panelUrl = document.getElementById("panelUrl").value
+          fetch("http://127.0.0.1:5000/api/posts", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+              "authorization": `Bearer ${token}`
+            },
+            body: JSON.stringify(data)
+          })
+            .then(response => response.json())
+            .then(result => {
+                console.log(result.message)
+                if (result.message && result.message === "Success") {
+                    navigate("/panel");
+                }
+            })
+            .catch(e => console.log(e))
+        })
+      }, [])
+
     console.log(token)
 
     return(
         <form action="http://127.0.0.1:5000/api/posts" method="POST">
             <label htmlFor="title" /> Title:
-            <input type="text" required={true} name="title" />
+            <input type="text" required={true} name="title" id="title" />
             <label htmlFor="text" /> Text:
-            <textarea required={true} name="text"></textarea>
+            <textarea required={true} name="text" id="text"></textarea>
+            <input type="hidden" name="panelUrl" value={window.location.href} id="panelUrl" />
             <button type="submit">Submit</button>
         </form>
     )
